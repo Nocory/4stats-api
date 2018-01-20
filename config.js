83800de8f6ef4e8826f0a58db7f0f230463fd9a3
Conroy <itsconroy+github@gmail.com>
2018-01-20 20:28:13 +0100

    updated client version and implemented ENV variable

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-	enforcedClientVersion: 10,
+	enforcedClientVersion: 12,
 	debugLevelDevelopment: "trace", // ["fatal","error","warn","info","debug","trace"]
 	debugLevelProduction: "info",	
-	popularThreads: 8
+	popularThreads: 8,
+	gathererURL: process.env.GATHERER_URL || "http://localhost:4002"
 }
 
 module.exports = config
