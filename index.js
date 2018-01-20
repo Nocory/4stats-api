0da5e629b8e78f961f3dc9f93de0bef0eaed8628
Conroy <itsconroy+github@gmail.com>
2018-01-20 20:28:59 +0100

    latest

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const config = require('./config')
 const pino = require('./pino')
-const axios = require('axios')
+//const axios = require('axios')
 
-const gathererURL = "http://localhost:4002"
+pino.info("process.env.NODE_ENV is %s",process.env.NODE_ENV)
+pino.info("process.env.DEBUG is %s",process.env.DEBUG)
+pino.info("process.env.PINO_LEVEL is %s",process.env.PINO_LEVEL)
+pino.info("config.gathererURL is %s",config.gathererURL)
 
 let boardStats = {}
 let activeThreads = {}
@@ -11,86 +14,167 @@ let history = {
 	day : {}
 }
 
-// Gatherer connection
-
-const gathererIO = require('socket.io-client')(gathererURL,{
+////////////////////
+// Gatherer setup //
+////////////////////
+const gathererIO = require('socket.io-client')(config.gathererURL,{
 	transports: ['websocket']
 })
 
-gathererIO.on("connect", socket => {
-	pino.info("gathererIO connected to gatherer server %s",gathererURL)
-})
-
-gathererIO.on("disconnect", socket => {
-	pino.error("gathererIO disconnected from gatherer server %s",gathererURL)
-})
-
-gathererIO.on("update", update => {
-	pino.debug("gathererIO received update for /%s/ history -> %j",update.board,Object.keys(update.history))
-	boardStats[update.board] = update.newBoardStats
-	activeThreads[update.board] = update.newActiveThreads
-	for(let term in update.history) history[term][update.board] = update.history[term]
-	serverIo.emit("boardUpdate",update.board,update.newBoardStats)
-})
-
-gathererIO.on("initialData", initialData => {
-	pino.info("gathererIO received initialData")
-	boardStats = initialData.liveBoardStats
-	activeThreads = initialData.activeThreads
-	history = initialData.history
-})
-
-
-// Client connection
-
+///////////////////////
+// API setup //
+///////////////////////
 const app = require('express')()
-const server = require('http').Server(app)
-const serverIo = require('socket.io')(server)
-const cors = require("cors")
+const server = require('http').createServer(app)
 
-app.use(cors())
+const corsOptions = {
+	origin: ["http://localhost:3000", "https://4stats.io", "https://4stats.moe"],
+}
+
+app.set('trust proxy', 'loopback')
+app.use(require("cors")(corsOptions))
 app.use(require('helmet')())
 app.use(require('compression')()) // TODO: not needed? Maybe nginx handles it by itself
 
-server.listen(4001)
+const apiIO = require('socket.io')(server)
+apiIO.origins(["localhost:*","4stats.io:*","4stats.moe:*"])
 
-serverIo.on('connection', socket => {
-	let ip = socket.request.headers["x-real-ip"] || socket.request.headers["x-forwarded-for"] || socket.handshake.address
-	pino.debug("New connection from %s",ip)
-	socket.emit("enforcedClientVersion",config.enforcedClientVersion)
+app.use(function (req, res, next) {
+	pino.info("%s %s %s",req.ip.padEnd(15," "),req.method,req.originalUrl)
+	next()
 })
 
+server.listen(4001)
+
+setInterval(() => {
+	apiIO.emit("userCount",apiIO.engine.clientsCount)
+},5316)
+
+/////////////////////////
+// Gatherer connection //
+/////////////////////////
+gathererIO.on("connect", () => {
+	pino.info("✓✓✓ gathererIO connected to %s",config.gathererURL)
+})
+
+gathererIO.on("disconnect", reason => {
+	pino.error("gathererIO disconnected from %s - %s",config.gathererURL,reason)
+})
+
+gathererIO.on("initialData", initialData => {
+	pino.info("✓✓✓ gathererIO received initialData")
+	boardStats = initialData.liveBoardStats
+	activeThreads = initialData.activeThreads
+	history = initialData.history
+	apiIO.emit("allBoardStats",boardStats)
+
+	gathererIO.on("update", update => {
+		pino.debug("gathererIO received update for /%s/ history -> %j",update.board,Object.keys(update.history))
+		boardStats[update.board] = update.newBoardStats
+		activeThreads[update.board] = update.newActiveThreads
+		for(let term in update.history) history[term][update.board] = update.history[term]
+		apiIO.emit("boardUpdate",update.board,update.newBoardStats)
+	})
+})
+
+////////////////////////////
+// API connection //
+////////////////////////////
+apiIO.on('connection', socket => {
+	let ip = socket.request.headers["x-real-ip"] || socket.request.headers["x-forwarded-for"] || socket.handshake.address
+	pino.info("%s Connected",ip.padEnd(15," "))
+	socket.emit("enforcedClientVersion",config.enforcedClientVersion)
+	socket.emit("allBoardStats",boardStats)
+	socket.on("disconnect",reason => {
+		pino.info("%s Disconnected %s",ip.padEnd(15," "),reason)
+	})
+})
+
+// NEW
+
+app.get('/all', function (req, res) {
+	res.send(boardStats)
+})
+
+app.get('/board/:board', function (req, res) {
+	res.send(boardStats[req.params.board])
+})
+
+app.get('/boards/:boards', function (req, res) {
+	const boardsArr = req.params.boards.split(",")
+	if(boardsArr.length > 10) return res.status(403).send('Query must not contain more than 10 boards')
+	let result = {}
+	for(let board of boardsArr){
+		result[board] = boardStats[board]
+	}
+	res.send(result)
+})
+
+app.get('/activeThreads/:board', function (req, res) {
+	res.send(activeThreads[req.params.board])
+})
+
+app.get('/history/:term/:board', (req, res) => {
+	if(history[req.params.term]){
+		res.send(history[req.params.term][req.params.board])
+	}else{
+		res.status(403).send('Invalid Query')
+	}
+})
+
+// OLD
+
 app.get('/allBoardStats', function (req, res) {
-	pino.debug("expressApp.get /allBoardStats ip: %s",req.get('x-real-ip') || req.ip)
 	res.send(boardStats)
 })
 
 app.get('/boardStats', function (req, res) {
-	pino.debug("expressApp.get /boardStats ip: %s query: %j",req.get('x-real-ip') || req.ip,req.query)
-	if(req.query.board && req.query.board.includes(",")){
-		let resultObj = {}
-		let boardQueries = req.query.board.split(",")
-		for(let board of boardQueries){
-			resultObj[board] = boardStats[board]
+	const board = req.query.board
+	const boards = req.query.boards
+
+	if(!board && !boards) return res.send(boardStats)
+	
+	if(boards){
+		const boardsArr = boards.split(",")
+		if(boardsArr.length > 10) return res.status(403).send('Query must not contain more than 10 boards')
+		let result = {}
+		for(let board of boardsArr){
+			result[board] = boardStats[board]
 		}
-		res.send(resultObj)
-	}else{
-		res.send(boardStats[req.query.board] || null)
+		return res.send(result)
 	}
+	
+	if(board && boardStats[board]){
+		return res.send(boardStats[board])
+	}
+	res.status(403).send('Invalid query string. Check the API.')
 })
 
 app.get('/activeThreads', function (req, res) {
-	pino.debug("expressApp.get /activeThreads ip: %s query: %s",req.get('x-real-ip') || req.ip,req.query.board)
 	res.send(activeThreads[req.query.board] || [])
 })
 
 app.get('/history', (req, res) => {
 	const board = req.query.board
+	const boards = req.query.boards
 	const term = req.query.term
-	pino.debug("expressApp.get /timeline ip: %s query: %j",req.get('x-real-ip') || req.ip,{board,term})
-	if(history[term]){
-		res.send(history[term][board] || [])
+
+	if(boards){
+		const boardsArr = boards.split(",")
+		//const boardsArr = boards.split(",").slice(0,10)
+		if(boardsArr.length > 10) return res.status(403).send('Query must not contain more than 10 boards')
+		let result = {}
+		for(let board of boardsArr){
+			if(history[term] && history[term][board]){
+				result[board] = history[term][board]
+			}
+		}
+		res.send(result)
 	}else{
-		res.send([])
+		if(history[term]){
+			res.send(history[term][board] || [])
+		}else{
+			res.send([])
+		}
 	}
 })
